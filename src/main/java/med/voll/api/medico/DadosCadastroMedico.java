6ecbc151493fe19cb627781d1ac109991828a54e
Guilherme <guilhermelemes08@gmail.com>
2023-06-11 19:54:39 -0300

    New migration: adding field to database

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -8,11 +8,14 @@ import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
-        @NotBlank                 //Anotação BeanValidation (checa se o nome não esta nulo ou vazio)
+        @NotBlank                 //Anotação BeanValidation (checa se o nome não esta nulo ou vazio) // Campo obrigatório
         String nome,
         @NotBlank
         @Email
         String email,
+
+        @NotBlank
+        String telefone,
         @NotBlank
         @Pattern(regexp = "\\d{4,6}")       //Anotação de Validação para dizer que o campo crm possui um numero padrão de 4 a 6 digitos
         String crm,
