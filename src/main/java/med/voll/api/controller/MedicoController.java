2f83b73dadf63511221f4ad47ded744810d77a66
Guilherme <guilhermelemes08@gmail.com>
2023-06-11 14:27:46 -0300

    Validations with Bean Validation

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
@@ -19,7 +20,7 @@ public class MedicoController {
     private MedicoRepository repository;
     @PostMapping
     @Transactional     //Anotação para indicar uma transação ativa com o banco de dados
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){ //RequestBody é uma notação que serve para informar o spring que o parametro do método é para pegar do corpo da requisição feita
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){ //RequestBody é uma notação que serve para informar o spring que o parametro do método é para pegar do corpo da requisição feita
         repository.save(new Medico(dados));
     }
 }
--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,6 +1,24 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+        @NotBlank                 //Anotação BeanValidation (checa se o nome não esta nulo ou vazio)
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")       //Anotação de Validação para dizer que o campo crm possui um numero padrão de 4 a 6 digitos
+        String crm,
+        @NotNull                            //Não é NotBlank pois não é uma String
+        Especialidade especialidade,
+        @NotNull
+        @Valid                              //Pede para o BeanValidation validar tambem os campos da DTO DadosEndereco
+        DadosEndereco endereco) {
 }
