30502541ddbabce0cb82cb9e31919b13ad09014c
Guilherme <guilhermelemes08@gmail.com>
2023-06-11 11:47:44 -0300

    Creating Repository Interface for JPA Entity

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){ //RequestBody é uma notação que serve para informar o spring que o parametro do método é para pegar do corpo da requisição feita
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 }
