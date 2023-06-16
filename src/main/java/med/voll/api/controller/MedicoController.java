2448d4474b362063a5260c2ca61cd27dd46a016f
Guilherme <guilhermelemes08@gmail.com>
2023-06-15 22:58:04 -0300

    GET requests, creating a method to list entries

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,13 +4,13 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,5 +22,10 @@ public class MedicoController {
     @Transactional     //Anotação para indicar uma transação ativa com o banco de dados
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){ //RequestBody é uma notação que serve para informar o spring que o parametro do método é para pegar do corpo da requisição feita
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList(); //convertemos uma lista de Medico para uma lista de DadosListagemMedico
     }
 }
