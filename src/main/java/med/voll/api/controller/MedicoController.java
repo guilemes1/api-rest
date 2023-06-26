0cef6ffd3715258be871135e0b9f060e8e685c66
Guilherme <guilhermelemes08@gmail.com>
2023-06-26 20:29:58 -0300

    Logical exclusion - inserting field in db to determine active or inactive user

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -26,7 +26,7 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemMedico::new); //convertemos uma lista de Medico para uma lista de DadosListagemMedico
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new); //convertemos uma lista de Medico para uma lista de DadosListagemMedico
     }
 
     @PutMapping
@@ -39,6 +39,7 @@ public class MedicoController {
     @DeleteMapping("/{id}")
     @Transactional
     public void excluir(@PathVariable Long id) {
-        repository.deleteById(id);
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 }
