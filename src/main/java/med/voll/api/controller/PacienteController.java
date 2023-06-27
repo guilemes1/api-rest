d999923fed4ff767f4ec62c7ae48d68bc42b059e
Guilherme <guilhermelemes08@gmail.com>
2023-06-27 19:08:41 -0300

    Entering data validation for customer registration

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
@@ -17,7 +18,7 @@ public class PacienteController {
     private PacienteRepository repository;
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
     }
 }
