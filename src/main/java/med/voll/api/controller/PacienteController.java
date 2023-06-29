b6459ba2b8f341e3ff96200ff92498623c5e20ee
Guilherme <guilhermelemes08@gmail.com>
2023-06-29 11:37:03 -0300

    Pagination and ordering of customers

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -6,6 +6,9 @@ import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,7 +26,7 @@ public class PacienteController {
     }
 
     @GetMapping
-    public List<DadosListagemPaciente> listar () {
-        return repository.findAll().stream().map(DadosListagemPaciente::new).toList();
+    public Page<DadosListagemPaciente> listar (@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
